eb57fead952b8fe438a26623400962fc74db7283
kpulks <123548501+kpulks@users.noreply.github.com>
2026-03-20 15:47:31 +0200

    Update portfolioData structure in data.js

--- data.js
+++ data.js
@@ -1,33 +1,29 @@
-const portfolioData = {
-    name: "Karoliina Pulkkinen",
-    title: "I am a philosopher and historian of science with an interest in how values influence scientific practice.",
-    bio: "Hej/Moi/Hello/Привет! I’m glad you found here. I’m currently a PI of the project “Tracking the role of values in Soviet science” at Aleksanteri Institute,  University of Helsinki. One of the aims of this research is to determine how past science can inform our normative frameworks regarding the management of social, political, and epistemic values in scientific practice. This research is supported by the Kone foundation.",
-    email: "karoliina.pulkkinen@ehelsinki.fi",
-    social: {
-        linkedin: "https://www.linkedin.com/in/karoliina-pulkkinen-ph-d-a9078915a/",
-        github: "https://github.com/yourprofile",
-        bluesky: "https://bsky.app/profile/kj-pulkkinen.bsky.social"
-    },
-    projects: [
-        {
-            title: " ",
-            description: "Alongside my interest in values, I also work on philosophy and history of chemistry and climate science. My previous postdoc concerned the role of values in climate modelling. The project "Values, Choices, and Uncertainties in Climate Modelling" was a collaboration between philosophers at KTH Royal Institute of Technology and climate scientists at Stockholm University.",
-            link: "https://link.springer.com/article/10.1007/s10584-022-03435-7"
-            link: "https://www.nature.com/articles/s41558-021-01238-9" 
-            
-    
-        },
-        {
-            title: " ",
-            description: "values in the development of periodic systems of chemical elements, including the system of the Russian chemist Dmitrii Mendeleev. The results of the research have been published in Philosophy of Science, Ambix, and Centaurus. I earned a PhD in History and Philosophy of Science at the University of Cambridge in 2019/2022.",
-            link: "https://www.cambridge.org/core/journals/philosophy-of-science/article/abs/value-of-completeness-how-mendeleev-used-his-periodic-system-to-make-predictions/E8E158C304390864141035EF9FB0AA3D"
-            link: "https://www.tandfonline.com/doi/full/10.1080/00026980.2020.1747325"
-
-
-        },
-        {
-          
+const portfolioData = [
+    {
+        name: "Karoliina Pulkkinen",
+        title: "I am a philosopher and historian of science with an interest in how values influence scientific practice.",
+        bio: "Hej/Moi/Hello/Привет! I'm glad you found here. I'm currently a PI of the project \"Tracking the role of values in Soviet science\" at Aleksanteri Institute, University of Helsinki. One of the aims of this research is to determine how past science can inform our normative frameworks regarding the management of social, political, and epistemic values in scientific practice. This research is supported by the Kone foundation.",
+        email: "karoliina.pulkkinen@ehelsinki.fi",
+        social: {
+            linkedin: "https://www.linkedin.com/in/karoliina-pulkkinen-ph-d-a9078915a/",
+            github: "https://github.com/kpulks",
+            bluesky: "https://bsky.app/profile/kj-pulkkinen.bsky.social"
         }
-    ],
-    
-};
+    },
+    {
+        title: "Philosophy and History of Chemistry & Climate Science",
+        description: "Alongside my interest in values, I also work on philosophy and history of chemistry and climate science. My previous postdoc concerned the role of values in climate modelling. The project \"Values, Choices, and Uncertainties in Climate Modelling\" was a collaboration between philosophers at KTH Royal Institute of Technology and climate scientists at Stockholm University.",
+        links: [
+            "https://link.springer.com/article/10.1007/s10584-022-03435-7",
+            "https://www.nature.com/articles/s41558-021-01238-9"
+        ]
+    },
+    {
+        title: "Periodic Systems of Chemical Elements & Mendeleev",
+        description: "Values in the development of periodic systems of chemical elements, including the system of the Russian chemist Dmitrii Mendeleev. The results of the research have been published in Philosophy of Science, Ambix, and Centaurus. I earned a PhD in History and Philosophy of Science at the University of Cambridge in 2019/2022.",
+        links: [
+            "https://www.cambridge.org/core/journals/philosophy-of-science/article/abs/value-of-completeness-how-mendeleev-used-his-periodic-system-to-make-predictions/E8E158C304390864141035EF9FB0AA3D",
+            "https://www.tandfonline.com/doi/full/10.1080/00026980.2020.1747325"
+        ]
+    }
+];
